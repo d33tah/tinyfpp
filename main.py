5928f469622528e4d4342afea150ab29c1eb5687
Jacek Wielemborek <d33tah@gmail.com>
2018-10-31 23:10:27 +0100

    Reorganize main.py.

--- main.py
+++ main.py
@@ -7,160 +7,92 @@ from OpenGL.GLUT import *
 TRIANGLES = []
 im = 0
 
-def load_maze():
+
+def append_node(x, z, c):
     global TRIANGLES
+    TRIANGLES.append(
+        [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 0.0, 'z': z},
+         {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 0.0, 'z': z},
+         {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 0.0, 'z': z + 1.0}
+         ])
+
+    TRIANGLES.append(
+        [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 0.0, 'z': z},
+         {'u': 0.0, 'v': 0.0, 'x': x, 'y': 0.0, 'z': z + 1.0},
+         {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 0.0, 'z': z + 1.0}
+         ]
+    )
+
+    TRIANGLES.append(
+        [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 1.0, 'z': z},
+         {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 1.0, 'z': z},
+         {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 1.0, 'z': z + 1.0}
+         ])
+
+    TRIANGLES.append(
+        [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 1.0, 'z': z},
+         {'u': 0.0, 'v': 0.0, 'x': x, 'y': 1.0, 'z': z + 1.0},
+         {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 1.0, 'z': z + 1.0}
+         ])
+
+    if c == ' ':
+        return
+
+    TRIANGLES.append(
+        [{'x': x + 1.0, 'y': 1.0, 'z': z, 'u': 0.0, 'v': 1.0},
+         {'x': x + 1.0, 'y': 0.0, 'z': z, 'u': 0.0, 'v': 0.0},
+         {'x': x, 'y': 0.0, 'z': z, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x + 1.0, 'y': 1.0, 'z': z, 'u': 0.0, 'v': 1.0},
+         {'x': x, 'y': 1.0, 'z': z, 'u': 1.5, 'v': 1.0},
+         {'x': x, 'y': 0.0, 'z': z, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x + 1.0, 'y': 1.0, 'z': z + 1.0, 'u': 0.0, 'v': 1.0},
+         {'x': x + 1.0, 'y': 0.0, 'z': z + 1.0, 'u': 0.0, 'v': 0.0},
+         {'x': x + 1.0, 'y': 0.0, 'z': z, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x + 1.0, 'y': 1.0, 'z': z + 1.0, 'u': 0.0, 'v': 1.0},
+         {'x': x + 1.0, 'y': 1.0, 'z': z, 'u': 1.5, 'v': 1.0},
+         {'x': x + 1.0, 'y': 0.0, 'z': z, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x, 'y': 1.0, 'z': z + 1.0, 'u': 0.0, 'v': 1.0},
+         {'x': x, 'y': 0.0, 'z': z + 1.0, 'u': 0.0, 'v': 0.0},
+         {'x': x, 'y': 0.0, 'z': z, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x, 'y': 1.0, 'z': z + 1.0, 'u': 0.0, 'v': 1.0},
+         {'x': x, 'y': 1.0, 'z': z, 'u': 1.5, 'v': 1.0},
+         {'x': x, 'y': 0.0, 'z': z, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x + 1.0, 'y': 1.0, 'z': z + 1.0, 'u': 0.0, 'v': 1.0},
+         {'x': x + 1.0, 'y': 0.0, 'z': z + 1.0, 'u': 0.0, 'v': 0.0},
+         {'x': x, 'y': 0.0, 'z': z + 1.0, 'u': 1.5, 'v': 0.0}
+         ])
+
+    TRIANGLES.append(
+        [{'x': x + 1.0, 'y': 1.0, 'z': z + 1.0, 'u': 0.0, 'v': 1.0},
+         {'x': x, 'y': 1.0, 'z': z + 1.0, 'u': 1.5, 'v': 1.0},
+         {'x': x, 'y': 0.0, 'z': z + 1.0, 'u': 1.5, 'v': 0.0}
+         ])
+
+
+def load_maze():
     with open('Data/maze.txt') as f:
         for x, line in enumerate(f):
             for z, c in enumerate(line.rstrip()):
+                append_node(x, z, c)
 
-                TRIANGLES.append(
-                    [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 0.0, 'z': z},
-                     {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 0.0, 'z': z},
-                     {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 0.0, 'z': z + 1.0}]
-                )
-
-                TRIANGLES.append(
-                    [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 0.0, 'z': z},
-                     {'u': 0.0, 'v': 0.0, 'x': x, 'y': 0.0, 'z': z + 1.0},
-                     {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 0.0, 'z': z + 1.0}]
-                )
-
-                TRIANGLES.append(
-                    [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 1.0, 'z': z},
-                     {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 1.0, 'z': z},
-                     {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 1.0, 'z': z + 1.0}]
-                )
-
-                TRIANGLES.append(
-                    [{'u': 0.0, 'v': 0.0, 'x': x, 'y': 1.0, 'z': z},
-                     {'u': 0.0, 'v': 0.0, 'x': x, 'y': 1.0, 'z': z + 1.0},
-                     {'u': 0.0, 'v': 0.0, 'x': x + 1.0, 'y': 1.0, 'z': z + 1.0}]
-                )
-
-                if c == ' ':
-                    continue
-
-                TRIANGLES.append([{'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x + 1.0,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 0.0,
-                                   'v': 0.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 1.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x + 1.0,
-                                   'y': 0.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 0.0},
-                                  {'x': x + 1.0,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 1.0},
-                                  {'x': x + 1.0,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 0.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 1.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x + 1.0,
-                                   'y': 0.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 0.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z + 1.0,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-                TRIANGLES.append([{'x': x + 1.0,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 0.0,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 1.0,
-                                   'z': z + 1.0,
-                                   'u': 1.5,
-                                   'v': 1.0},
-                                  {'x': x,
-                                   'y': 0.0,
-                                   'z': z + 1.0,
-                                   'u': 1.5,
-                                   'v': 0.0}])
-    return TRIANGLES
 
 def setup_textures():
     global im
@@ -172,16 +104,17 @@ def setup_textures():
     im = glGenTextures(1)
     glBindTexture(GL_TEXTURE_2D, im)
 
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, textureData)
 
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
-    camera_x, camera_y, camera_z = (-0.5,0.5,0.5)
+    camera_x, camera_y, camera_z = (-0.5, 0.5, 0.5)
     xtrans, ytrans, ztrans = -camera_x, -camera_y, -camera_z
     glTranslatef(xtrans, ytrans, ztrans)
 
@@ -192,17 +125,18 @@ def display():
 
     glBindTexture(GL_TEXTURE_2D, im)
 
+    glBegin(GL_TRIANGLES)
+    glNormal3f(0.0, 0.0, 1.0)
     for triangle in TRIANGLES:
-        glBegin(GL_TRIANGLES)
-        glNormal3f( 0.0, 0.0, 1.0)
 
         for i in range(3):
             t = triangle[i]
             glTexCoord2f(t['u'], t['v'])
             glVertex3f(t['x'], t['y'], t['z'])
 
-        glEnd()
+    glEnd()
     glutSwapBuffers()
+
 
 def initGL():
     glEnable(GL_TEXTURE_2D)
@@ -216,14 +150,14 @@ def initGL():
 
 
 def main():
-    TRIANGLES = load_maze()
+    load_maze()
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
     glutCreateWindow('interactive')
     glutDisplayFunc(display)
     setup_textures()
     initGL()
-    glutIdleFunc(display);
+    glutIdleFunc(display)
     glutMainLoop()
 
 
